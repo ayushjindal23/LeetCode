5aa96fd3ddf3cf9250c51eee1d30315c920f1184
Ayush Jindal <64908612+ayushjindal23@users.noreply.github.com>
2022-05-31 08:20:17 +0530

    Update 1461. Check If a String Contains All Binary Codes of Size K.cpp

--- May Leetcode/1461. Check If a String Contains All Binary Codes of Size K.cpp
+++ May Leetcode/1461. Check If a String Contains All Binary Codes of Size K.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     bool hasAllCodes(string s, int k) {
-	int n = s.size(); 
-	for(int num = 0; num <= (1 << k); num++){
-		string binaryCode = bitset<20>(num).to_string().substr(20-k, k);
-		if(s.find(binaryCode) == string::npos) return false;
-	}        
-	return true;
-}
+        if (k > s.size()) return false;
+        
+        unordered_set<string> my_set;
+        
+        for (int i = 0; i <= s.size()-k; i++)
+            my_set.insert(s.substr(i, k));
+        
+        return my_set.size() == pow(2, k);
+    }
 };
