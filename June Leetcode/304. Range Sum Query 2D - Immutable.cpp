34bd44dc9ffe8a4546099f15102c746ef1a3cd86
Ayush Jindal <64908612+ayushjindal23@users.noreply.github.com>
2022-06-03 08:44:41 +0530

    Update 304. Range Sum Query 2D - Immutable.cpp

--- June Leetcode/304. Range Sum Query 2D - Immutable.cpp
+++ June Leetcode/304. Range Sum Query 2D - Immutable.cpp
@@ -1,30 +1,17 @@
 class NumMatrix {
 public:
-    class NumMatrix {
-private:
-    int row, col;
-    vector<vector<int>> sums;
-public:
-    NumMatrix(vector<vector<int>> &matrix) {
-        row = matrix.size();
-        col = row>0 ? matrix[0].size() : 0;
-        sums = vector<vector<int>>(row+1, vector<int>(col+1, 0));
-        for(int i=1; i<=row; i++) {
-            for(int j=1; j<=col; j++) {
-                sums[i][j] = matrix[i-1][j-1] + 
-                             sums[i-1][j] + sums[i][j-1] - sums[i-1][j-1] ;
+    vector<vector<int>> sum;
+    NumMatrix(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        sum = vector<vector<int>>(m + 1, vector<int>(n + 1)); // sum[i][j] is sum of all elements inside the rectangle [0,0,i,j]
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + matrix[i - 1][j - 1];
             }
         }
     }
-
-    int sumRegion(int row1, int col1, int row2, int col2) {
-        return sums[row2+1][col2+1] - sums[row2+1][col1] - sums[row1][col2+1] + sums[row1][col1];
+    int sumRegion(int r1, int c1, int r2, int c2) {
+        r1++; c1++; r2++; c2++; // Since our `sum` starts by 1 so we need to increase r1, c1, r2, c2 by 1
+        return sum[r2][c2] - sum[r2][c1 - 1] - sum[r1 - 1][c2] + sum[r1 - 1][c1 - 1];
     }
 };
-};
-
-/**
- * Your NumMatrix object will be instantiated and called as such:
- * NumMatrix* obj = new NumMatrix(matrix);
- * int param_1 = obj->sumRegion(row1,col1,row2,col2);
- */
