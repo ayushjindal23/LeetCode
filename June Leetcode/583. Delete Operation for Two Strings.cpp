0d5c0c2f4415474c48f1e68a58a894348d550ad2
Ayush Jindal <64908612+ayushjindal23@users.noreply.github.com>
2022-06-14 15:18:26 +0530

    Update 583. Delete Operation for Two Strings.cpp

--- June Leetcode/583. Delete Operation for Two Strings.cpp
+++ June Leetcode/583. Delete Operation for Two Strings.cpp
@@ -1,13 +1,25 @@
-vector<vector<int> >dp;
-int minDistance(string& word1, string& word2) {
-	// dp[i][j] will denote minimum steps required to equalize word1[i:end] and word[2:j:end]
-	dp.resize(size(word1) + 1, vector<int>(size(word2) + 1, 1000));
-	return solve(word1, word2, 0, 0);
-}
-int solve(string &w1, string &w2, int i, int j) {
-	if(i == size(w1) && j == size(w2)) return 0;
-	if(i == size(w1) || j == size(w2)) return max(size(w1) - i, size(w2) - j);
-	if(dp[i][j] != 1000) return dp[i][j];  // directly return stored answer if already computed before
-	if(w1[i] == w2[j]) return solve(w1, w2, i + 1, j + 1);
-	return dp[i][j] = 1 + min(solve(w1, w2, i + 1, j), solve(w1, w2, i, j + 1));
-}
+class Solution {
+public:
+    
+    int minDistance(string word1, string word2) 
+    {
+        int m=word1.length(), n=word2.length(); 
+        
+        vector<vector<int>> dp(m+1, vector<int> (n+1, 0)); 
+        
+        for(int i=0; i<=m; i++)
+        {
+            for(int j=0; j<=n; j++)
+            {
+                if(i==0 || j==0) continue;
+                
+                else if(word1[i-1]==word2[j-1])
+                    dp[i][j] = 1+dp[i-1][j-1];
+                
+                else
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return m+n-2*dp[m][n];
+    }
+};
